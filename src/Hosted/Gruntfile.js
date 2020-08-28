6b3bd972cc350702eef6ef070c77fc42b85e9006
jooseppi12 <urijozsef@europe.com>
2020-08-28 12:29:02 +0200

    Remove unneeded stuff

--- src/Hosted/Gruntfile.js
+++ src/Hosted/Gruntfile.js
@@ -2,38 +2,17 @@ module.exports = function(grunt) {
 	const sass = require('node-sass');
 	 
 	grunt.initConfig({
-	    sass: {
-	        options: {
-	            implementation: sass
-	        },
-	        dist: {
-	            files: {
-	                'css/all.css': 'scss/all.scss'
-	            }
-	        }
-	    },
 	    cssmin: {
 			build: {
 			  src: 'css/all.css',
 			  dest: 'css/all.min.css'
 			}
-		},
-	    uglify: { 
-	        options: { 
-	            compress: true 
-	        }, 
-	        applib: { 
-	            src: [ 
-	                'node_modules/@creativebulma/bulma-collapsible/dist/js/bulma-collapsible.min.js' 
-	            ], 
-	            dest: 'js/bulma-collapsible.js' 
-	        } 
-	    } 
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	 
-	grunt.registerTask('default', ['sass', 'cssmin', 'uglify']);
+	grunt.registerTask('default', ['cssmin']);
 };
