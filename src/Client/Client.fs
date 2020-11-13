8fe1ae4aef1720b19495edbbd664ddb53bdbc3d7
jooseppi12 <urijozsef@europe.com>
2020-11-13 15:24:04 +0100

    Use .attr isnted of .prop

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -41,7 +41,7 @@ module Newsletter =
         JQuery.JQuery.Of("#signUp").Click(fun _ ev ->
             JQuery.JQuery.Of(".newsletter-form .success-box").RemoveClass("visible").Ignore
             JQuery.JQuery.Of(".newsletter-form .error-box").RemoveClass("visible").Ignore
-            JQuery.JQuery.Of("#signUp").AddClass("loading").Prop("disabled", true).Ignore
+            JQuery.JQuery.Of("#signUp").AddClass("loading").Attr("disabled", "disabled").Ignore
             let email : string = JQuery.JQuery.Of("#nemail").Val() :?> string
             if email.Trim() <> "" then
                 let fd = FormData()
@@ -56,17 +56,17 @@ module Newsletter =
                         Type = JQuery.RequestType.POST,
                         Success = (fun _ _ _ ->
                             JQuery.JQuery.Of(".newsletter-form .success-box").AddClass("visible").Ignore
-                            JQuery.JQuery.Of("#signUp").RemoveClass("loading").RemoveProp("disabled").Ignore
+                            JQuery.JQuery.Of("#signUp").RemoveClass("loading").RemoveAttr("disabled").Ignore
                         ),
                         Error = (fun _ _ _ ->
                             JQuery.JQuery.Of(".newsletter-form .error-box").AddClass("visible").Ignore
-                            JQuery.JQuery.Of("#signUp").RemoveClass("loading").RemoveProp("disabled").Ignore
+                            JQuery.JQuery.Of("#signUp").RemoveClass("loading").RemoveAttr("disabled").Ignore
                         )
                     )
                 JQuery.JQuery.Ajax(ajaxSettings) |> ignore
                 ev.PreventDefault()
             else
-                JQuery.JQuery.Of("#signUp").RemoveClass("loading").RemoveProp("disabled").Ignore
+                JQuery.JQuery.Of("#signUp").RemoveClass("loading").RemoveAttr("disabled").Ignore
         ).Ignore
 
 [<SPAEntryPoint>]
